e6522b2939fc1d8d8aebdb8da410c009729d5428
higasimaru <andyourbirdcansing@icloud.com>
2020-04-08 20:35:41 +0900

    flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations', sessions: 'users/sessions', passwords: 'users/passwords'}
   root 'tweets#index'
+  resources :users, only: [:edit, :update]
 end
