910c932290008315cb2a8dcbf115ce252fd28e79
higasimaru <andyourbirdcansing@icloud.com>
2020-04-10 17:33:15 +0900

    tweet_form

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,14 +1,29 @@
 class TweetsController < ApplicationController
+  before_action :set_movie
   def index
-    
+    @tweet = Tweet.new
+    @tweets = @movie.tweets.includes(:user)
   end
 
   def create
+    @tweet = @movie.tweets.new(tweet_params)
+    if @tweet.save
+      redirect_to movie_tweets_path(@movie), notice: 'コメントしました！'
+    else
+      @tweets = @movie.tweets.includes(:user)
+      flash.now[:alert] = '送信できませんでした'
+      render :index
+    end
   end
 
   private
 
   def tweet_params
-    params.require()
+    params.require(:tweet).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id]) 
+  end
   
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,6 @@ class Tweet < ApplicationRecord
   belongs_to :movie
 
   validates :content, presence: true, unless: :image?
+
+  mount_uploader :image, ImageUploader
 end
