0d88278be0e47224b3e6a8ea090fe16db0c2f114
higasimaru <andyourbirdcansing@icloud.com>
2020-04-11 13:31:45 +0900

    content, image 表示

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,16 @@ class Movie < ApplicationRecord
   has_many :tweets
 
   validates :name, presence: true, uniqueness: true
+
+  def  show_last_tweet
+    if (last_tweet = tweets.last).present?
+      if last_tweet.image?
+        last_tweet.image
+      else
+        last_tweet.content
+      end
+    else
+      'まだコメントがありません' 
+    end
+  end
 end
