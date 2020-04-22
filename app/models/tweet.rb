810dadce5e92e747935d6f8920f72a2796c25a96
higasimaru <andyourbirdcansing@icloud.com>
2020-04-22 11:46:19 +0900

    tweets_controller

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,4 +5,9 @@ class Tweet < ApplicationRecord
   validates :content, presence: true, unless: :image?
 
   mount_uploader :image, ImageUploader
+
+  def self.search(input)
+    return nil if input == ''
+    Tweet.where('content LIKE(?)', "%#{input}%")
+  end
 end
