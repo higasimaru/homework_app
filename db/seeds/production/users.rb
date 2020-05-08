3e0e995ee2f42f7efb38b769d0409fde858e3e42
higasimaru <andyourbirdcansing@icloud.com>
2020-05-08 17:14:27 +0900

    Set user seed data

---
+++ db/seeds/production/users.rb
@@ -0,0 +1,16 @@
+100.times do |n|
+  name = ('a'..'z').to_a.shuffle[0..5].join
+  user = User.find_or_initialize_by(
+    nickname: "#{name}",
+    email: "user#{n+1}@me.com",
+    # activated: true
+  )
+
+  if user.new_record?
+    user.password = "password"
+    password_confirmation = "password"
+    user.save!
+  end
+end
+
+puts "user = #{User.count}"
